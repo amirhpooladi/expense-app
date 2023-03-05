1762f92c2a66e0b180a36936c1ca55400b81c54f
amirhosseinpooladi <amirhosseinpooladi@yahoo.com>
2023-03-05 14:16:21 +0330

    changes

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import "../components/ExpenseItem"
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Marxh 28th 2021</div>
+      <h2>car insurance</h2>
+      <div>$294.67</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
